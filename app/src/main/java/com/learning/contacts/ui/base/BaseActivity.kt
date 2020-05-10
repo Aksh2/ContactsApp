bb931fc5a5221deb76cecb9961a116882633eaca
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-10 20:39:10 +0530

    Initial commit for BaseActivity.

---
+++ app/src/main/java/com/learning/contacts/ui/base/BaseActivity.kt
@@ -0,0 +1,41 @@
+package com.learning.contacts.ui.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.android.AndroidInjection
+import javax.inject.Inject
+
+/**
+ * Abstract activity which binds ViewModel [VM] and ViewBinding [VB]
+ */
+abstract class BaseActivity<VM : ViewModel, VB : ViewBinding> : AppCompatActivity() {
+
+    @Inject
+    protected lateinit var mViewModelProvider: ViewModelProvider.Factory
+
+    protected val mViewModel by lazy { getViewModel() }
+
+    protected lateinit var mViewBinding: VB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+        super.onCreate(savedInstanceState)
+
+        mViewBinding = getViewBinding()
+    }
+
+
+    /**
+     * returns VB which is assigned to mViewBinding in onCreate
+     */
+
+    abstract fun getViewBinding() : VB
+
+    /**
+     * returns VM which is assigned to mViewModel
+     */
+
+    abstract fun getViewModel() : VM
+}
