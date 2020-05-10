b189ca980a05171bf99c06f032e3e91a2ac671f2
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-10 20:57:00 +0530

    Initial commit for contact service.

---
+++ app/src/main/java/com/learning/contacts/network/ContactsService.kt
@@ -0,0 +1,19 @@
+package com.learning.contacts.network
+
+import com.learning.contacts.model.Contact
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ContactsService {
+
+    @GET("/contacts")
+    suspend fun getContacts()
+
+    @POST("/contacts")
+    suspend fun postContacts(@Body contact: Contact)
+
+    companion object {
+        const val CONTACTS_SERVICE_BASE_URL="http://167.172.6.138"
+    }
+}
