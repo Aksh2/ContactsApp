3ddef074ad9e27a1c0dfd0861390ad3c4fed5968
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-12 12:04:27 +0530

    refactor: initial commit for activity builder.

---
+++ app/src/main/java/com/learning/contacts/di/builder/ActivityBuilder.kt
@@ -0,0 +1,15 @@
+package com.learning.contacts.di.builder
+
+import com.learning.contacts.ui.contactdetails.ContactDetailActivity
+import com.learning.contacts.ui.main.MainActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class ActivityBuilder {
+    @ContributesAndroidInjector
+    abstract fun bindMainActivity(): MainActivity
+
+    @ContributesAndroidInjector
+    abstract fun bindContactDetailActivity(): ContactDetailActivity
+}
