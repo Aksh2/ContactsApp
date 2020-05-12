42da4d678aafc917a388acc43a86edb295a3a5e2
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-12 12:26:55 +0530

    feature: initial commit for contact detail activity.

---
+++ app/src/main/java/com/learning/contacts/ui/contactdetails/ContactDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.learning.contacts.ui.contactdetails
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.learning.contacts.R
+
+class ContactDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_details)
+    }
+}
