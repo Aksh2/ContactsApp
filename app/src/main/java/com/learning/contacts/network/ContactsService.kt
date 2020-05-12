003bc4b43c68fe4f755302d92551d0755745f0c8
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-12 12:11:01 +0530

    refactor: Changed the return type and the url.

--- app/src/main/java/com/learning/contacts/network/ContactsService.kt
+++ app/src/main/java/com/learning/contacts/network/ContactsService.kt
@@ -1,6 +1,7 @@
 package com.learning.contacts.network
 
 import com.learning.contacts.model.Contact
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -8,12 +9,12 @@ import retrofit2.http.POST
 interface ContactsService {
 
     @GET("/contacts")
-    suspend fun getContacts()
+    suspend fun getContacts(): Response<List<Contact>>
 
     @POST("/contacts")
     suspend fun postContacts(@Body contact: Contact)
 
     companion object {
-        const val CONTACTS_SERVICE_BASE_URL="http://167.172.6.138"
+        const val CONTACTS_SERVICE_BASE_URL = "http://167.172.6.138:8080"
     }
 }
