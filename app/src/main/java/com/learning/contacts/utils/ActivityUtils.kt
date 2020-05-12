ecc293a6d470c952aa78c695ee606ef597086f55
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-12 12:26:18 +0530

    feature: initial commit for activity utils.

---
+++ app/src/main/java/com/learning/contacts/utils/ActivityUtils.kt
@@ -0,0 +1,8 @@
+package com.learning.contacts.utils
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+inline fun <reified VM : ViewModel> AppCompatActivity.viewModelOf(factory: ViewModelProvider.Factory) =
+    ViewModelProvider(this, factory).get(VM::class.java)
