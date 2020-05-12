5baf0aa783ea11d48cd622175051405de40994a6
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-12 12:10:25 +0530

    refactor: Added conflict strategy and helper method to insert a list of contacts.

--- app/src/main/java/com/learning/contacts/data/database/ContactsDao.kt
+++ app/src/main/java/com/learning/contacts/data/database/ContactsDao.kt
@@ -2,6 +2,7 @@ package com.learning.contacts.data.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.learning.contacts.model.Contact
 import kotlinx.coroutines.flow.Flow
@@ -11,6 +12,9 @@ interface ContactsDao {
        @Insert
        fun insertContact(contact: Contact)
 
-        @Query("SELECT * FROM ${Contact.CONTACTS_TABLE_NAME}")
-        fun getAllContacts(): Flow<List<Contact>>
+       @Insert(onConflict = OnConflictStrategy.REPLACE)
+       fun insertContactList(contacts: List<Contact>)
+
+       @Query("SELECT * FROM ${Contact.CONTACTS_TABLE_NAME}")
+       fun getAllContacts(): Flow<List<Contact>>
 }
