f2778b9363d7e3acea21c75a1c11d10510639b38
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-13 12:34:58 +0530

    refactor: added fetching of contacts from repository.

--- app/src/main/java/com/learning/contacts/ui/contactdetails/ContactDetailsViewModel.kt
+++ app/src/main/java/com/learning/contacts/ui/contactdetails/ContactDetailsViewModel.kt
@@ -1,6 +1,12 @@
 package com.learning.contacts.ui.contactdetails
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.learning.contacts.data.repository.ContactRepository
+import javax.inject.Inject
 
-class ContactDetailsViewModel : ViewModel() {
+class ContactDetailsViewModel @Inject constructor(private val contactsRepository: ContactRepository) :
+    ViewModel() {
+
+    fun getContactById(id: Int) = contactsRepository.getContactById(id).asLiveData()
 }
