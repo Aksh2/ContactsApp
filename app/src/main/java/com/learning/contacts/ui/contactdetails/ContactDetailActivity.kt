5bcad50cdaee1e038f7c019b9654ef02d06143ae
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-13 12:28:36 +0530

    refactor: add viewbinding for contactdetailactivity.

--- app/src/main/java/com/learning/contacts/ui/contactdetails/ContactDetailActivity.kt
+++ app/src/main/java/com/learning/contacts/ui/contactdetails/ContactDetailActivity.kt
@@ -1,13 +1,46 @@
 package com.learning.contacts.ui.contactdetails
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.learning.contacts.R
+import androidx.lifecycle.Observer
+import com.learning.contacts.databinding.ActivityContactDetailsBinding
+import com.learning.contacts.ui.base.BaseActivity
+import com.learning.contacts.utils.viewModelOf
 
-class ContactDetailActivity : AppCompatActivity() {
+class ContactDetailActivity :
+    BaseActivity<ContactDetailsViewModel, ActivityContactDetailsBinding>() {
+
+    companion object {
+        const val CONTACT_ID = "contactId"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact_details)
+        setContentView(mViewBinding.root)
+        title = "Details"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val contactId = intent.extras?.getInt(CONTACT_ID)
+            ?: throw IllegalArgumentException("Contact Id must not be null")
+
+        initialiseContactDetails(contactId)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    fun initialiseContactDetails(contactId: Int) {
+        mViewModel.getContactById(contactId).observe(this, Observer {
+            mViewBinding.tvFirstName.text = it.firstname
+            mViewBinding.tvLastName.text = it.lastname
+            mViewBinding.tvEmail.text = it.email
+            mViewBinding.tvMobile.text = it.phone
+        })
+    }
+
+    override fun getViewBinding(): ActivityContactDetailsBinding =
+        ActivityContactDetailsBinding.inflate(layoutInflater)
+
+    override fun getViewModel(): ContactDetailsViewModel =
+        viewModelOf<ContactDetailsViewModel>(mViewModelProvider)
 }
