06f5b7c80bbaf564a4074140568800de94260b5c
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-13 21:42:49 +0530

    feature: Added helper functions.

--- app/src/main/java/com/learning/contacts/utils/ActivityUtils.kt
+++ app/src/main/java/com/learning/contacts/utils/ActivityUtils.kt
@@ -1,8 +1,18 @@
 package com.learning.contacts.utils
 
+import android.app.Activity
+import android.content.Context
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 inline fun <reified VM : ViewModel> AppCompatActivity.viewModelOf(factory: ViewModelProvider.Factory) =
     ViewModelProvider(this, factory).get(VM::class.java)
+
+fun Activity.getColorRes(id: Int) = ContextCompat.getColor(applicationContext, id)
+
+fun Activity.showToast(context: Context, message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(context, message, duration).show()
+}
