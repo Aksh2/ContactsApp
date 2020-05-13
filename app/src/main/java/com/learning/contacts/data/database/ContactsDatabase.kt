75959122be4a4857865d9d1f82af16482f7a3e1f
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-13 12:36:43 +0530

    refactor: removed harcoding of values.

--- app/src/main/java/com/learning/contacts/data/database/ContactsDatabase.kt
+++ app/src/main/java/com/learning/contacts/data/database/ContactsDatabase.kt
@@ -4,19 +4,22 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.learning.contacts.data.database.ContactsDatabase.Companion.CONTACT_DATABASE_VERSION
+import com.learning.contacts.data.database.ContactsDatabase.Companion.EXPORT_SCHEME
 import com.learning.contacts.model.Contact
 
 @Database(
     entities = [Contact::class],
-    version = 1,
-    exportSchema = false
+    version = CONTACT_DATABASE_VERSION,
+    exportSchema = EXPORT_SCHEME
 )
 abstract class ContactsDatabase : RoomDatabase() {
-
     abstract fun getContactsDao(): ContactsDao
 
     companion object{
         const val DB_NAME = "contacts_database"
+        const val CONTACT_DATABASE_VERSION = 1
+        const val EXPORT_SCHEME = false
 
         @Volatile
         private var INSTANCE: ContactsDatabase? = null
