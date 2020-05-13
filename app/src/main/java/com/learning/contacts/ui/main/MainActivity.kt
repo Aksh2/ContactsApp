e2d41aab4c2fd6d4064dc6547da55b020818e731
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-13 12:39:39 +0530

    fix: update fetching of contacts when activity is opened. refactor: removed unused code.

--- app/src/main/java/com/learning/contacts/ui/main/MainActivity.kt
+++ app/src/main/java/com/learning/contacts/ui/main/MainActivity.kt
@@ -3,18 +3,14 @@ package com.learning.contacts.ui.main
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.ImageView
-import androidx.core.app.ActivityOptionsCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.snackbar.Snackbar
-import com.learning.contacts.R
 import com.learning.contacts.databinding.ActivityMainBinding
 import com.learning.contacts.model.Contact
 import com.learning.contacts.network.ResponseState
 import com.learning.contacts.ui.adapter.ContactAdapter
+import com.learning.contacts.ui.addcontact.AddContactActivity
 import com.learning.contacts.ui.base.BaseActivity
 import com.learning.contacts.ui.contactdetails.ContactDetailActivity
 import com.learning.contacts.utils.viewModelOf
@@ -22,7 +18,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>(),
     ContactAdapter.OnItemClickListener {
-
+    val TAG = MainActivity::class.java.simpleName
     private val mAdapter: ContactAdapter by lazy { ContactAdapter(onItemClickListener = this) }
 
     override fun getViewBinding(): ActivityMainBinding =
@@ -41,11 +37,15 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>(),
         }
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            startActivity(Intent(this, AddContactActivity::class.java))
         }
 
         initContacts()
+        mViewModel.getContacts()
+    }
+
+    override fun onResume() {
+        super.onResume()
         mViewModel.getContacts()
     }
 
@@ -53,47 +53,23 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>(),
         mViewModel.postContactLiveData.observe(this, Observer { state ->
             when (state) {
                 is ResponseState.Loading -> {
-                    Log.d("MAIN", "Loading")
+                    Log.d(TAG, "Loading")
                 }
                 is ResponseState.Success -> {
-                    Log.d("contacrs", " data ${state.data}")
+                    Log.d(TAG, " data ${state.data}")
                     mAdapter.submitList(state.data.toMutableList())
                 }
                 is ResponseState.Error -> {
-                    Log.d("Error", "error: ${state.message}")
-
+                    Log.d(TAG, "error: ${state.message}")
                 }
             }
         })
 
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onItemClicked(contact: Contact, imageView: ImageView) {
         val intent = Intent(this, ContactDetailActivity::class.java)
-        intent.putExtra("id", contact.id)
-
-        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
-            this,
-            imageView,
-            imageView.transitionName
-        )
-
-        startActivity(intent, options.toBundle())
+        intent.putExtra(ContactDetailActivity.CONTACT_ID, contact.id)
+        startActivity(intent)
     }
 }
